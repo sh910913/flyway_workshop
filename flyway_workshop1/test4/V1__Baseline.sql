a0d3232f024f135763d4eacde2a82aa321c83414
gordon.liao <sh910913@gmai.com>
2022-01-02 23:02:52 +0800

    init

---
+++ flyway_workshop1/test4/V1__Baseline.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person (
+    ID int not null,
+    NAME varchar(100) not null
+);
+INSERT INTO person (ID, NAME) VALUES (1, 'Axel');
+INSERT INTO person (ID, NAME) VALUES (2, 'Mr. Foo');
+INSERT INTO person (ID, NAME) VALUES (3, 'Ms. Bar');
